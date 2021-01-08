c3681afbbec7bf420b4cfb3480694828bed55aaf
Bart Siwek <bartlomiej.siwek@gmail.com>
2021-01-08 21:47:11 +0100

    Add missing files

---
+++ graphics/include/gris/graphics/vulkan/vma_headers.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VULKAN_HPP_TYPESAFE_CONVERSION
+#define VULKAN_HPP_NO_EXCEPTIONS
+#include <vk_mem_alloc.hpp>
---
+++ graphics/include/gris/graphics/vulkan/vulkan_headers.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4100)
+#endif
+
+#define VULKAN_HPP_TYPESAFE_CONVERSION
+#define VULKAN_HPP_NO_EXCEPTIONS
+#include <vulkan/vulkan.hpp>
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
