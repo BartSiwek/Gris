fd5b75a46d8c4a50f9798e3ef301b01d8bdc9360
Bart Siwek <bartlomiej.siwek@gmail.com>
2021-01-08 22:14:33 +0100

    Remove comments

--- graphics/include/gris/graphics/vulkan/vulkan_headers.h
+++ graphics/include/gris/graphics/vulkan/vulkan_headers.h
@@ -2,20 +2,7 @@
 
 #include <gris/assert.h>
 
-/*
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable : 4100)
-#endif
-*/
-
 #define VULKAN_HPP_ASSERT(cond) GRIS_FAST_ASSERT(cond, "Vulkan Hpp asser failed")
 #define VULKAN_HPP_TYPESAFE_CONVERSION
 #define VULKAN_HPP_NO_EXCEPTIONS
 #include <vulkan/vulkan.hpp>
-
-/*
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
-*/
