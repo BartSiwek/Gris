61ad525515349fe64ed74952046b6f8dce1bebdf
Bart Siwek <bartlomiej.siwek@gmail.com>
2021-01-10 23:30:47 +0100

    Windows fixes

--- graphics/src/gris/graphics/vulkan/vma_implementation.cpp
+++ graphics/src/gris/graphics/vulkan/vma_implementation.cpp
@@ -1,2 +1,15 @@
 #define VMA_IMPLEMENTATION
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4100)
+#pragma warning(disable : 4127)
+#pragma warning(disable : 4296)
+#pragma warning(disable : 4324)
+#endif
+
 #include <vk_mem_alloc.h>
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
