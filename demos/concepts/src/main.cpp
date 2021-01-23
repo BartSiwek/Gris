12707a1123acfab1f0f8e82809c4ae0cbecc3a7a
Bart Siwek <bartlomiej.siwek@gmail.com>
2021-01-23 21:25:15 +0100

    Small program to test concepts

---
+++ demos/concepts/src/main.cpp
@@ -0,0 +1,39 @@
+#include <gris/log.h>
+#include <iostream>
+
+template<typename TLoggerImpl>
+concept LoggerLike = requires(TLoggerImpl log)
+{
+    log.LogDebug(std::string_view{});
+    log.LogInfo(std::string_view{});
+    log.LogError(std::string_view{});
+};
+
+template<LoggerLike TLoggerImpl>
+struct Logger : public TLoggerImpl
+{};
+
+struct MyLoggerImpl
+{
+    void LogDebug(std::string_view message) const
+    {
+        std::cout << "[My Debug] " << message << '\n';
+    }
+    void LogInfo(std::string_view message) const
+    {
+        std::cout << "[My Info] " << message << '\n';
+    }
+    void LogError(std::string_view message) const
+    {
+        std::cout << "[My Error] " << message << '\n';
+    }
+};
+
+using MyLogger = Logger<MyLoggerImpl>;
+
+int main()
+{
+    LoggerLike auto logger = MyLogger{};
+    logger.LogDebug("Hello demo!");
+    return EXIT_SUCCESS;
+}
