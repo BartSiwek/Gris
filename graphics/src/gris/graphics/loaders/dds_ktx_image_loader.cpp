697a5101826b6eb343185d2dc5d5eef6e696132e
Bart Siwek <bartlomiej.siwek@gmail.com>
2021-04-28 19:54:47 +0200

    GCC warnings fix

--- graphics/src/gris/graphics/loaders/dds_ktx_image_loader.cpp
+++ graphics/src/gris/graphics/loaders/dds_ktx_image_loader.cpp
@@ -16,8 +16,8 @@ Gris::Graphics::Image Gris::Graphics::Loaders::DdsKtxImageLoader::Load(const std
 {
     auto fileContents = Gris::ReadFile<uint8_t>(path);
 
-    auto tc = ddsktx_texture_info{ 0 };
-    ddsktx_error err = {};
+    auto tc = ddsktx_texture_info{};
+    auto err = ddsktx_error{};
     if (!ddsktx_parse(&tc, fileContents.data(), static_cast<int>(fileContents.size()), &err))
     {
         throw EngineException("Failed to load DDS texture image " + path.string(), err.msg);
@@ -26,16 +26,16 @@ Gris::Graphics::Image Gris::Graphics::Loaders::DdsKtxImageLoader::Load(const std
     // TODO: Make this a lot more flexible and so
     GRIS_ALWAYS_ASSERT(tc.format == DDSKTX_FORMAT_BC2, "Format must be DDSKTX_FORMAT_BC2");
 
-    ddsktx_sub_data sub_data;
+    auto sub_data = ddsktx_sub_data{};
     ddsktx_get_sub(&tc, &sub_data, fileContents.data(), static_cast<int>(fileContents.size()), 0, 0, 0);
 
     Image result;
-    result.PixelData.resize(sub_data.size_bytes);
+    result.PixelData.resize(static_cast<size_t>(sub_data.size_bytes));
     result.Width = static_cast<uint32_t>(sub_data.width);
     result.Height = static_cast<uint32_t>(sub_data.height);
     result.Format = ImageFormat::BC2;
 
-    std::memcpy(result.PixelData.data(), sub_data.buff, sub_data.size_bytes);
+    std::memcpy(result.PixelData.data(), sub_data.buff, static_cast<size_t>(sub_data.size_bytes));
 
     return result;
 }
